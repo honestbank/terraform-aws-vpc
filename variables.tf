5cc624c5968a40bbe601e7e0ba4ef2fe8004254d
Peter Griffin <peter.griffin@honestbank.com>
2021-07-26 18:39:48 +0700

    update variable types

--- variables.tf
+++ variables.tf
@@ -6,13 +6,13 @@ variable "cidr" {
 }
 
 variable "public_subnets" {
-
+  type = list(string)
 }
 
 variable "private_subnets" {
-
+  type = list(string)
 }
 
 variable "azs" {
-
+  type = list(string)
 }
