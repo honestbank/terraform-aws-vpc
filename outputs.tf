8617b323be478989fa4f9bd32eeb0cab3fa7d04e
Peter Griffin <peter.griffin@honestbank.com>
2021-07-30 19:19:08 +0700

    updated outputs.tf per PR request

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "private_subnets" {
-  value = module.vpc.private_subnets
+  description = "List of private subnet IDs returned from terraform-aws-vpc module (aws_subnet.private.*.id)"
+  value       = module.vpc.private_subnets
 }
 
 output "public_subnets" {
-  value = module.vpc.public_subnets
+  description = "List of public subnet IDs, returned from terraform-aws-vpc module (aws_subnet.public.*.id)"
+  value       = module.vpc.public_subnets
 }
 
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  description = "AWS identifier for the VPC provisioned by this module"
+  value       = module.vpc.vpc_id
 }
