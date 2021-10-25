d3146146bbdddb5fa37c5cc9a03aeba38b43f6dc
Jai Govindani <jai@honestbank.com>
2021-10-25 20:16:31 +0700

    feat: add support for VPC flow logs
    Signed-off-by: Jai Govindani <jai@honestbank.com>

--- aws-vpc-wrapper.tf
+++ aws-vpc-wrapper.tf
@@ -21,10 +21,12 @@ terraform {
 module "aws-vpc" {
   source = "./aws-vpc"
 
-  name = var.name
-
-  cidr            = var.cidr
+  name            = var.name
   azs             = var.azs
+  cidr            = var.cidr
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
+
+  enable_flow_log                                 = var.enable_flow_log
+  flow_log_cloudwatch_log_group_retention_in_days = var.flow_log_cloudwatch_log_group_retention_in_days
 }
--- aws-vpc/inputs.tf
+++ aws-vpc/inputs.tf
@@ -1,9 +1,21 @@
 variable "name" {
   description = "Name allocated to the VPC. Used as the VPC name and as a prefix to other items, for example subnets"
+  type        = string
 }
 
 variable "cidr" {
   description = "The CIDR allocation for the VPC. Largest is /16, smallest is /28. We use /16 to supply 8 x /19 subnets (6 active, 2 reserved for future expansion)"
+  type        = string
+}
+
+variable "enable_flow_log" {
+  description = "Enable VPC flow logs"
+  type        = bool
+}
+
+variable "flow_log_cloudwatch_log_group_retention_in_days" {
+  description = "The VPC flow log retention period in days"
+  type        = number
 }
 
 variable "public_subnets" {
