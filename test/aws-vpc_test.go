018032c02dd3e640de9f8ce208129d8e0169a2a1
Jai Govindani <jai@honestbank.com>
2021-11-07 17:21:43 +0700

    test: add dummy AWS provider
    Signed-off-by: Jai Govindani <jai@honestbank.com>

--- test/aws-vpc_test.go
+++ test/aws-vpc_test.go
@@ -1,14 +1,17 @@
 package test
 
 import (
+	"io"
+	"log"
+	"os"
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/logger"
 	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/logger"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 )
 
 func TestTerraformAwsVpc(t *testing.T) {
@@ -19,7 +22,48 @@ func TestTerraformAwsVpc(t *testing.T) {
 	public_subnets := []string{"10.0.0.0/19"}
 	private_subnets := []string{"10.0.128.0/19"}
 
-	workingDir := test_structure.CopyTerraformFolderToTemp(t, "..", "aws-vpc")
+	moduleFolder := "aws-vpc"
+	workingDir := test_structure.CopyTerraformFolderToTemp(t, "..", moduleFolder)
+
+	// Copy a dummy AWS provider to suppress 'region' warnings
+	src := "aws-provider.tf"
+	log.Printf("stat-ing source file: %s", src)
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		log.Printf("error accessing file: %s, failing test NOW", src)
+		t.FailNow()
+	}
+
+	log.Printf("checking if %s is regular file", src)
+	if !sourceFileStat.Mode().IsRegular() {
+		log.Printf("%s is not a regular file, failing test NOW", src)
+		t.FailNow()
+	}
+
+	log.Printf("opening source file: %s", src)
+	source, err := os.Open(src)
+	if err != nil {
+		log.Printf("error opening file: %s, failing test NOW", src)
+		t.FailNow()
+	}
+	defer source.Close()
+
+	destFile := workingDir + "/" + src
+	log.Printf("creating destination file: %s", destFile)
+	destination, err := os.Create(destFile)
+	if err != nil {
+		log.Printf("error creating destination: %s, failing test NOW", destFile)
+		t.FailNow()
+	}
+	defer destination.Close()
+
+	log.Printf("copying file from source: %s to destination: %s", src, destFile)
+	_, err = io.Copy(destination, source)
+	if err != nil {
+		log.Printf("error copying source: %s to destination: %s, failing test NOW", src, destFile)
+		t.FailNow()
+	}
+
 	logger.Logf(t, "path to test folder %s\n", workingDir)
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
