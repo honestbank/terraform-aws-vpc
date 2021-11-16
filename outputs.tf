149fb1ae97ae8c1a41261e36c785b05ef063e0e4
Jai Govindani <jai@honestbank.com>
2021-11-16 17:04:59 +0700

    feat: add route table outputs
    Signed-off-by: Jai Govindani <jai@honestbank.com>

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,16 @@
+output "private_route_table_ids" {
+  description = "List of IDs of private route tables."
+  value       = module.aws-vpc.private_route_table_ids
+}
+
 output "private_subnets" {
   description = "List of private subnet IDs returned from terraform-aws-vpc module (aws_subnet.private.*.id)"
   value       = module.aws-vpc.private_subnets
+}
+
+output "public_route_table_ids" {
+  description = "List of IDs of public route tables."
+  value       = module.aws-vpc.public_route_table_ids
 }
 
 output "public_subnets" {
